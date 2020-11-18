b940aca23fcc50838ff92c3e72a0ab3d07854557
yukiabineko <yukiabineko@live.com>
2020-11-18 18:38:04 +0900

    'test'

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+export default function App(){
+  return(
+    <div>sample</div>
+  )
+}
 
-    this.state = {
-      text: null
-    };
-  }
 
-  componentDidMount() {
+
+/*
+componentDidMount() {
     const fetchInit = {
       method: "GET",
       headers: { "content-type": "application/json" }
@@ -21,25 +20,4 @@ class App extends Component {
       .then(response => response.json())
       .then(response => this.setState(response));
   }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Response: {this.state.text}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+*/
