072ba01e9bec77b0b0740b7600f931e7580cea27
yukiabineko <yuki1980426@gmail.com>
2020-11-20 08:21:23 +0900

    'newFormadd'

--- src/users/New.jsx
+++ src/users/New.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
 import '../App.css';
 import { withRouter } from 'react-router'
 
 const  New = (props)=>{
 
+  const[state, setState] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password_confirmatin: '',
+    data: []
+  })
+
   const loginComponent = ()=>{
     props.history.push('/login')  
+  }
+  const userInput = (e)=>{
+    const target = e.target.value;
+    const name = e.target.name;
+    const value = e.target.value;
+    setState({...state, [name]: value});
+  }
+  const sendUserParameter = (e)=>{
+    e.preventDefault();
+    alert(JSON.stringify(state));
   }
   return(
    <>
@@ -22,25 +40,25 @@ const  New = (props)=>{
         >
          戻る
         </Button>
-          <Form>
+          <Form onSubmit={sendUserParameter}>
             <Form.Group>
               <Form.Label>お名前</Form.Label>
-              <Form.Control type="text" name="name" placeholder="*必須です。" className="h8" required />
+              <Form.Control type="text" name="name" placeholder="*必須です。" className="h8" required onChange={userInput} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>メールアドレス</Form.Label>
-              <Form.Control type="email" name="email" placeholder="*必須です。" required />
+              <Form.Control type="email" name="email" placeholder="*必須です。" required onChange={userInput} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>パスワード</Form.Label>
-              <Form.Control type="password" name="password" placeholder="*必須です。" required />
+              <Form.Control type="password" name="password" placeholder="*必須です。" required onChange={userInput} />
             </Form.Group>
 
             <Form.Group>
               <Form.Label>パスワード確認</Form.Label>
-              <Form.Control type="password" name="password_confirmation" placeholder="*もう一度入力ください。" required />
+              <Form.Control type="password" name="password_confirmation" placeholder="*もう一度入力ください。" required onChange={userInput} />
             </Form.Group>
 
               <Button 
