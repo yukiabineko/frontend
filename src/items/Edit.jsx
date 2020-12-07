34ea5358e51f2760e22b42c962d3a1f31da66281
yukiabineko <yukiabineko@live.com>
2020-12-07 13:55:31 +0900

    fix

--- src/items/Edit.jsx
+++ src/items/Edit.jsx
@@ -3,13 +3,21 @@ import { Row, Col, Form, Button } from 'react-bootstrap';
 import { withRouter } from 'react-router';
 import axios from 'axios';
 import '../users/users.css';
-import { getitemData } from './setItemData';
 
 /**************************************************************************************** */
 const  ItemEdit = (props)=>{
 
- let item = getitemData();
-
+  const getitemData = ()=>{
+    let item = []
+    let datas = JSON.parse(localStorage.getItem('items'));
+    datas.forEach((data)=>{
+      if(data.id === props.id){
+       item.push(data);
+      }
+    });
+    return item
+   }
+   let item = getitemData();
 /*********************************state******************************************************* */
   const[show,setShow] =useState({
     display: 'none'
