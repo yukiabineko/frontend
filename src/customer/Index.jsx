96e0319ad3b1e6d4d94ec7623b4bbbff957f5b93
yukiabineko <yuki1980426@gmail.com>
2020-12-10 23:34:23 +0900

    'fix'

--- src/customer/Index.jsx
+++ src/customer/Index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Row, Col, Table, Button } from 'react-bootstrap';
+import { Row, Col, Table, Button,Image } from 'react-bootstrap';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 import '../App.css'
@@ -8,6 +8,14 @@ import image from '../images/fishs2.jpg';
 
 
  function Index(props){
+  let localData = JSON.parse(localStorage.getItem('items'))
+  let modalData = [];
+
+
+  /************************ステート*************************************** */
+  const[itemData, setState] = useState(
+    localData ? localData : []
+  )
  
 /******************************ログイン/未ログイン切り替え********************************************************** */
     const loginUserCheck = ()=>{
@@ -26,7 +34,21 @@ import image from '../images/fishs2.jpg';
        <div className="customor-main">
     　　　<h1 className="customor-main-title">本日入荷商品紹介</h1>
          <article>
-           <h5>商品一覧</h5>
+           <section class="gallery">
+             <h2>本日入荷商品</h2>
+             {itemData.length > 0 ? 
+               <ul>
+                 {itemData.map((item)=>(
+                   <li>
+                       <Image src={`http://yukiabineko.sakura.ne.jp/react/${item.name}.jpg`} alt="表示できません" roundedCircle />
+                   </li>
+                 ))}
+               </ul>
+                :
+               ''
+              }
+            
+           </section>
 
          </article>
          <div className="customor-right">
