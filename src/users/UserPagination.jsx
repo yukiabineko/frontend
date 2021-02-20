17c018b0c03e3c54263d892f884836ca53522133
abineko2 <yuki1980426@gmail.com>
2021-02-21 01:28:19 +0900

    2/21ユーザーページネーション

---
+++ src/users/UserPagination.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { withRouter } from 'react-router';
+import { Pagination } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+
+const  UserPagination = (props)=>{
+    let paginationLength = JSON.parse(localStorage.getItem('users')).length;
+    let active = props.No +1;
+    let items = [];
+    if(props.No > 0){
+       items.push(
+        <Pagination.First onClick={()=>backButton()} /> 
+       )
+    }
+    for (let number = 1; number <= paginationLength /2; number++) {
+      items.push(
+        <Pagination.Item key={number} active={number === active} onClick={(num)=>sendNumber(number-1)}>
+          {number}
+        </Pagination.Item>,
+      );
+    }
+  /************************ページネーション処理**************************************** */
+  const sendNumber =(num)=>{
+      props.paginationSend(num);
+  }
+  /************************ページネーション戻るボタン処理**************************************** */
+  const backButton = () =>{
+     alert(props.No -1);
+  }
+  return(
+   <>
+    <Pagination >{items}</Pagination>
+   </>
+  )
+}
+export default withRouter(connect((state)=>state)(UserPagination))
