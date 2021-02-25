1d8ec1caa3e50df3a2922a59ad3274cf041a232b
植松勇貴 <uematsuyuuki@uematsuyuukinoMacBook-Pro.local>
2021-02-25 17:27:21 +0900

    2/25データ不具合対策

--- src/users/UserPagination.jsx
+++ src/users/UserPagination.jsx
@@ -9,50 +9,55 @@ import { customers } from './setting';
 const  UserPagination = (props)=>{
   /*********************ステート******************************** */
   const[group, setGroup] = useState(0);
-    let paginationLength =  customers(JSON.parse(localStorage.getItem('users'))).length;
+    let localData = JSON.parse(localStorage.getItem('users'))
+   
     let active = props.No +1;
     let items = [];
-    
-    if(group === 0 &&  Math.round(paginationLength) > 5 && Math.round(paginationLength / 2) > 5 ){  /*初期のページネーション データ5以上 かつ表示ボタン5以下*/
-      for (let number = 1; number <= 5; number++) {
+
+
+
+    if(localData){
+      let paginationLength =  customers(localData).length;
+      if(group === 0 &&  Math.round(paginationLength) > 5 && Math.round(paginationLength / 2) > 5 ){  /*初期のページネーション データ5以上 かつ表示ボタン5以下*/
+        for (let number = 1; number <= 5; number++) {
+          items.push(
+            <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
+              {number}
+            </Pagination.Item>,
+          );
+        }
         items.push(
-          <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
-            {number}
-          </Pagination.Item>,
+          <Pagination.Last onClick={()=>groupPlus()}/>
         );
       }
-      items.push(
-        <Pagination.Last onClick={()=>groupPlus()}/>
-      );
+      else if(group === 0 &&  Math.round(paginationLength) <5 || Math.round(paginationLength / 2) < 5 ){   /*初期のページネーション データ5以下 または表示ボタン5以下*/
+        for (let number = 1; number <= Math.round(paginationLength /2); number++) {
+          items.push(
+            <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
+              {number}
+            </Pagination.Item>,
+          );
+        }
+      }
+      else if(group >=1){
+        items.push(
+          <Pagination.First onClick={()=>groupMinus()} />
+        );
+        for (let number = group * 5 + 1; number <= Math.round(paginationLength /2) * group; number++) {
+          items.push(
+            <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
+              {number}
+            </Pagination.Item>,
+          );
+        }
+        if(Math.round(paginationLength /2) *group * 5 <= paginationLength ){ /*５の倍数で分けているため数字x5が実際のユーザー数より多ければ進む表示*/
+          items.push(
+            <Pagination.Last onClick={()=>groupPlus()} />
+          );
+        }
+        
+      }
     }
-    else if(group === 0 &&  Math.round(paginationLength) <5 || Math.round(paginationLength / 2) < 5 ){   /*初期のページネーション データ5以下 または表示ボタン5以下*/
-      for (let number = 1; number <= Math.round(paginationLength /2); number++) {
-        items.push(
-          <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
-            {number}
-          </Pagination.Item>,
-        );
-      }
-    }
-    else if(group >=1){
-      items.push(
-        <Pagination.First onClick={()=>groupMinus()} />
-      );
-      for (let number = group * 5 + 1; number <= Math.round(paginationLength /2) * group; number++) {
-        items.push(
-          <Pagination.Item key={number} active={number === active} onClick={()=>sendNumber(number-1)}>
-            {number}
-          </Pagination.Item>,
-        );
-      }
-      if(Math.round(paginationLength /2) *group * 5 <= paginationLength ){ /*５の倍数で分けているため数字x5が実際のユーザー数より多ければ進む表示*/
-        items.push(
-          <Pagination.Last onClick={()=>groupPlus()} />
-        );
-      }
-      
-    }
-    
   /************************ページネーション処理**************************************** */
   const sendNumber =(num)=>{
       props.paginationSend(num);
